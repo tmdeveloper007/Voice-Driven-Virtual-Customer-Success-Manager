568ba9eaa7d83a33f4e90351a4630022ef9f7b4c
Arpita Verma <a8729060@gmail.com>
2026-06-24 11:32:25 +0530

    Remove BlockchainService and its initialization method
    Removed unused BlockchainService initialization code.

--- src/main/java/com/vcsm/VcsmApplication.java
+++ src/main/java/com/vcsm/VcsmApplication.java
@@ -3,16 +3,11 @@ package com.vcsm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-@Autowired
-private BlockchainService blockchainService;
 
-@PostConstruct
-public void initBlockchain() {
-    blockchainService.initBlockchain();
-}
 @SpringBootApplication
 @EnableScheduling
 public class VcsmApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(VcsmApplication.class, args);
     }
